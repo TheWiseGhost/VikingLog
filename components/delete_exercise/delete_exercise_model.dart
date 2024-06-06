63842952641043e08569ab64ab5657def4bbfeee
Aditya Byju <155671746+TheWiseGhost@users.noreply.github.com>
2024-06-06 14:17:28 -0500

    Create delete_exercise_model.dart

---
+++ components/delete_exercise/delete_exercise_model.dart
@@ -0,0 +1,19 @@
+import '/auth/supabase_auth/auth_util.dart';
+import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'delete_exercise_widget.dart' show DeleteExerciseWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class DeleteExerciseModel extends FlutterFlowModel<DeleteExerciseWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
