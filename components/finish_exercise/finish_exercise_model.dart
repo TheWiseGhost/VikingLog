a90b152162cfa9025b8063c41eaf6a89e42c9e6b
Aditya Byju <155671746+TheWiseGhost@users.noreply.github.com>
2024-06-06 14:19:00 -0500

    Create finish_exercise_model.dart

---
+++ components/finish_exercise/finish_exercise_model.dart
@@ -0,0 +1,23 @@
+import '/auth/supabase_auth/auth_util.dart';
+import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'finish_exercise_widget.dart' show FinishExerciseWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+
+class FinishExerciseModel extends FlutterFlowModel<FinishExerciseWidget> {
+  ///  State fields for stateful widgets in this component.
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
