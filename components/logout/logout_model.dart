b5a5aaf387f7ad66aa180aa6e3dbea4b9d4d681f
Aditya Byju <155671746+TheWiseGhost@users.noreply.github.com>
2024-06-06 14:20:12 -0500

    Create logout_model.dart

---
+++ components/logout/logout_model.dart
@@ -0,0 +1,21 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'logout_widget.dart' show LogoutWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+
+class LogoutModel extends FlutterFlowModel<LogoutWidget> {
+  ///  State fields for stateful widgets in this component.
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
