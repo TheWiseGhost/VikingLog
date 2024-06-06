358b2d0522e8041c6aeb30421c3f167d60ad2da4
Aditya Byju <155671746+TheWiseGhost@users.noreply.github.com>
2024-06-06 14:21:14 -0500

    Create paid_plan_prompt_model.dart

---
+++ components/paid_plan_prompt/paid_plan_prompt_model.dart
@@ -0,0 +1,21 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'paid_plan_prompt_widget.dart' show PaidPlanPromptWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+
+class PaidPlanPromptModel extends FlutterFlowModel<PaidPlanPromptWidget> {
+  ///  State fields for stateful widgets in this component.
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
