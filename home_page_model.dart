292abcde96a36c0573c527bc7dc3aa0abf8d4751
Aditya Byju <155671746+TheWiseGhost@users.noreply.github.com>
2024-06-06 13:14:11 -0500

    Create home_page_model.dart

---
+++ home_page_model.dart
@@ -0,0 +1,56 @@
+import '/auth/supabase_auth/auth_util.dart';
+import '/backend/supabase/supabase.dart';
+import '/components/app_title/app_title_widget.dart';
+import '/components/create_exercise/create_exercise_widget.dart';
+import '/components/exercise_on_tap/exercise_on_tap_widget.dart';
+import '/components/paid_plan_prompt/paid_plan_prompt_widget.dart';
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:math';
+import 'dart:async';
+import 'home_page_widget.dart' show HomePageWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // Model for AppTitle component.
+  late AppTitleModel appTitleModel;
+  Completer<List<ExercisesRow>>? requestCompleter;
+
+  @override
+  void initState(BuildContext context) {
+    appTitleModel = createModel(context, () => AppTitleModel());
+  }
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    appTitleModel.dispose();
+  }
+
+  /// Additional helper methods.
+  Future waitForRequestCompleted({
+    double minWait = 0,
+    double maxWait = double.infinity,
+  }) async {
+    final stopwatch = Stopwatch()..start();
+    while (true) {
+      await Future.delayed(Duration(milliseconds: 50));
+      final timeElapsed = stopwatch.elapsedMilliseconds;
+      final requestComplete = requestCompleter?.isCompleted ?? false;
+      if (timeElapsed > maxWait || (requestComplete && timeElapsed > minWait)) {
+        break;
+      }
+    }
+  }
+}
