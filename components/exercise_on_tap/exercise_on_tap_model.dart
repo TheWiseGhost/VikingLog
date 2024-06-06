b6c6a8fe9180b0c6ed48fb28b1ffbe3060d1efa4
Aditya Byju <155671746+TheWiseGhost@users.noreply.github.com>
2024-06-06 14:18:29 -0500

    Create exercise_on_tap_model.dart

---
+++ components/exercise_on_tap/exercise_on_tap_model.dart
@@ -0,0 +1,20 @@
+import '/backend/supabase/supabase.dart';
+import '/components/delete_exercise/delete_exercise_widget.dart';
+import '/components/edit_exercise/edit_exercise_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'exercise_on_tap_widget.dart' show ExerciseOnTapWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class ExerciseOnTapModel extends FlutterFlowModel<ExerciseOnTapWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
